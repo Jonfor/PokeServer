5cd6b018e57fc41802ca7ac0c95205fd85081b36
jonfor <jonforscher@gmail.com>
2015-10-02 18:44:14 -0400

    Added freq. and serv. tables and SQL insertions into these tables.

--- index.js
+++ index.js
@@ -19,59 +19,95 @@ var server = app.listen(8001, function() {
 });
 
 var dbFile = 'test.db';
-var db = new sqlite3.Database(dbFile, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, function (err) {
+var db = new sqlite3.Database(dbFile, sqlite3.OPEN_READWRITE, function (err) {
     if (err) throw err;
     console.log("Opened database %s successfully", dbFile);
 });
 
-app.get('/registration/:token', function (req, res, next) {
+app.post('/registration', function (req, res, next) {
     db.serialize(function() {
-        //db.run("CREATE TABLE test (token TEXT)");
-        db.get("SELECT rowid AS id, token FROM test", function(err, row) {
+
+        /* Return a message if the token is already in the database.
+         * Otherwise, insert all the data into the database. */
+        db.get("SELECT rowid AS id, token FROM users", function (err, row) {
             if (err) throw err;
-            if (row === undefined) {
-                next();
-            } else {
-                res.send("Token already exists in database.");
-                db.all("SELECT rowid AS id, token FROM test", function(err, rows) {
+            if (row && (row.token === req.body.token)) {
+                res.send("Registration token already exists in database.");
+                db.all("SELECT rowid AS id, token FROM Users", function (err, rows) {
                     if (err) throw err;
                     console.log(rows);
                 });
+            } else {
+                next();
             }
         });
     });
 }, function (req, res, next) {
     db.serialize(function() {
-        var stmt = db.prepare("INSERT INTO test VALUES (?)");
+        console.log("Token: " + req.body.token);
+        console.log("Frequency: " + req.body.frequency);
+        console.log("Server info: " + req.body.ip, req.body.port);
 
-        console.log("Token: " + req.params.token);
-        stmt.run(req.params.token, [], function(err) {
+        var stmtUsers = db.prepare("INSERT INTO users VALUES (?)");
+        stmtUsers.run(req.body.token, [], function (err) {
             if (err) throw err;
 
-            stmt.finalize();
-            res.send("Inserted new token into database.");
-            db.all("SELECT rowid AS id, token FROM test", function(err, rows) {
+            stmtUsers.finalize();
+            db.all("SELECT rowid AS id, token FROM users", function (err, rows) {
                 if (err) throw err;
-                console.log(rows);
+                console.log("TOKENS" + JSON.stringify(rows));
             });
         });
+
+        var stmtFrequencies = db.prepare("INSERT INTO frequencies (frequency) VALUES($frequency)");
+        stmtFrequencies.run(
+            {
+                $frequency: req.body.frequency
+            }, function (err) {
+                if (err) throw err;
+                stmtFrequencies.finalize();
+                db.all("SELECT rowid AS id, frequency FROM frequencies", function (err, rows) {
+                    if (err) throw err;
+                    console.log("FREQUENCIES: " + JSON.stringify(rows));
+                });
+            });
+
+        var stmtServers = db.prepare("INSERT INTO servers (ip, port) VALUES($ip, $port)");
+        stmtServers.run(
+            {
+                $ip: req.body.ip,
+                $port: req.body.port
+            }, function (err) {
+                if (err) throw err;
+
+                stmtServers.finalize();
+                db.all("SELECT rowid AS id, ip FROM servers", function (err, rows) {
+                    if (err) throw err;
+                    console.log("SERVERS: " + JSON.stringify(rows));
+                });
+            });
+
+        res.send("Inserted new token, frequency, & server into database.");
     });
 });
 
+/**
+ * Send message out to users.
+ */
 app.get('/message', function (req, res, next) {
-    db.get("SELECT rowid AS id, token FROM test", function(err, row) {
+    db.get("SELECT rowid AS id, token FROM users", function (err, row) {
         if (err) throw err;
 
         var message = new gcm.Message();
         message.addData('key1', 'msg1');
 
-        var regIds = [row.token];
+        var tokens = [row.token];
 
         // Set up the sender with you API key
         var sender = new gcm.Sender('AIzaSyA6ZKrlTMd8zxu8Tc0t8OXn4wSvNR6RS0E');
 
         // Now the sender can be used to send messages
-        sender.send(message, { registrationIds: regIds }, function (err, result) {
+        sender.send(message, {tokens: tokens}, function (err, result) {
             if (err) {
                 console.error(err);
                 res.status(400).send("An error occurred.");
